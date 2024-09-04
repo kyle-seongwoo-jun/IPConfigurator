732a9dda4978bc3ff4122e34d9a71c47cc9bea95
Kyle Seongwoo Jun <njk230@gmail.com>
2024-09-04 15:23:02 +0900

    작업중이던 changes

--- IPConfigurator.Console/Program.cs
+++ IPConfigurator.Console/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IPConfigurator.Controllers;
+using static System.Console;
 
 namespace IPConfigurator.Console
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string command = args[0];
+
+			if (command == "list")
+			{
+				var configurator = NetworkAdapterConfigurator.Instance;
+
+				var adapters = configurator.NetworkAdapters;
+
+				foreach (var item in adapters)
+				{
+					WriteLine(item);
+				}
+			}
+		}
+	}
+
+	interface ICommandLineInteractive
+	{
+		string OnListCommand();
+
+		string OnHelpCommand();
+	}
 }
